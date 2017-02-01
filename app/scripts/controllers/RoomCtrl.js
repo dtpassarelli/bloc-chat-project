03ae4b1de1a1516ea1e8e135583007b22b95bd61
Dan Passarelli <danpassarelli@Dans-MacBook-Pro.local>
2017-02-01 12:08:35 -0700

    added controllers having trouble with ng-repeat and the all method

---
+++ app/scripts/controllers/RoomCtrl.js
@@ -0,0 +1,9 @@
+(function() {
+    function RoomCtrl() {
+        var $scope = $scope.all ;
+    }
+ 
+     angular
+         .module('bloc-chat-project')
+         .controller('RoomCtrl', [RoomCtrl]);
+ })();
